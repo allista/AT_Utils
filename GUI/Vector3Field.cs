e8ec570228ad8253ad9aa8f79a4f964212d5f482
Allis Tauri <allista@gmail.com>
2016-11-13 16:22:54 +0300

    Added Vector3Field GUI for editing Vector3 components.

---
+++ GUI/Vector3Field.cs
@@ -0,0 +1,53 @@
+﻿//   Vector3Field.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+
+using System;
+using UnityEngine;
+
+namespace AT_Utils
+{
+	public class Vector3Field
+	{
+		public FloatField Fx = new FloatField();
+		public FloatField Fy = new FloatField();
+		public FloatField Fz = new FloatField();
+
+		public Vector3 Value
+		{
+			get { return new Vector3(Fx.Value, Fy.Value, Fz.Value); }
+			set 
+			{
+				Fx.Value = value.x;
+				Fy.Value = value.y;
+				Fz.Value = value.z;
+			}
+		}
+
+		public bool UpdateValue()
+		{
+			var updated = false;
+			updated = Fx.UpdateValue() || updated;
+			updated = Fy.UpdateValue() || updated;
+			updated = Fz.UpdateValue() || updated;
+			return updated;
+		}
+
+		public bool Draw(string name, string suffix = "", bool show_set_button = true, float increment = 0, string iformat = "F1")
+		{
+			var ret = false;
+			GUILayout.BeginHorizontal();
+			GUILayout.Label(name, GUILayout.ExpandWidth(false));
+			ret = Fx.Draw("", false, increment, iformat) || ret;
+			ret = Fy.Draw("", false, increment, iformat) || ret;
+			ret = Fz.Draw(suffix, show_set_button, increment, iformat) || ret;
+			if(Fz.IsSet) ret = UpdateValue() || ret;
+			GUILayout.EndHorizontal();
+			return ret;
+		}
+	}
+}
+
