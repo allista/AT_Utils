49ed78a8d5ad05c5cee9eb8b260094985d5ab8d1
Allis Tauri <allista@gmail.com>
2016-11-13 19:39:58 +0300

    Do not include AnimatedConverters into the release.

--- make-release.sh
+++ make-release.sh
@@ -3,5 +3,7 @@
 cd $(dirname "$0")
 
 ../../PyKSPutils/make_mod_release \
--e '*.user' '*.orig' '*.mdb' 'GameData/ConfigurableContainers/Parts/*' \
+-e '*.user' '*.orig' '*.mdb' \
+'GameData/ConfigurableContainers/Parts/*' \
+'*/AnimatedConverters.dll' \
 -i './ConfigurableContainers/GameData'
