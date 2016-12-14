e472deaccb80ee758ad30055fb010d7055335330
Allis Tauri <allista@gmail.com>
2016-12-14 21:22:52 +0300

    Improved ResourceTransferWindow performance and usability.

--- GUI/ResourceTransferWindow.cs
+++ GUI/ResourceTransferWindow.cs
@@ -25,48 +25,48 @@ namespace AT_Utils
 
 		public ResourceTransferWindow()
 		{
-			width = 360;
+			width = 600;
 			height = 100;
 		}
 		
-		static float ResourceLine(string label, float fraction, 
-		               		      double pool, 
-		               		      double minAmount, double maxAmount, 
-		               		      double capacity)
+		static float ResourceLine(string label, float fraction, ResourceManifest res)
 		{
 			GUILayout.BeginHorizontal ();
 
 			// Resource name
 			GUILayout.Box(label, Styles.white, GUILayout.Width(120), GUILayout.Height(res_line_height));
+			var rhs = fraction*res.maxAmount;
+			var lhs = res.pool-rhs;
+			// lhs amount/capacity
+			GUILayout.Box(Utils.formatBigValue((float)res.host_capacity, "u"),
+			              Styles.yellow, GUILayout.Width(60), GUILayout.Height(res_line_height));
+			GUILayout.Box(Utils.formatBigValue((float)lhs, "u"),
+			              Styles.fracStyle((float)(lhs/res.host_capacity)), 
+			              GUILayout.Width(60), GUILayout.Height(res_line_height));
 			// Fill amount
 			// limit slider to 0.5% increments
 			GUILayout.BeginVertical();
-			fraction = GUILayout.HorizontalSlider(fraction, 0.0F, 1.0F,
+			GUILayout.Box((fraction * 100) + "%", Styles.slider_text, 
+			              GUILayout.Width(200), GUILayout.Height(20));
+			var frac = GUILayout.HorizontalSlider(fraction, 0.0F, 1.0F,
 												  Styles.slider,
 												  GUI.skin.horizontalSliderThumb,
-												  GUILayout.Width(300),
+			                                      GUILayout.Width(200),
 												  GUILayout.Height(20));
-			
-			fraction = (float)Math.Round(fraction, 3);
-			fraction = (Mathf.Floor(fraction * 200)) / 200;
-			if(fraction*maxAmount < minAmount) fraction =(float)(minAmount/maxAmount);
-			GUILayout.Box((fraction * 100) + "%",
-						   Styles.slider_text, GUILayout.Width(300),
-						   GUILayout.Height(20));
+			frac = (float)Math.Round(frac, 3);
+			frac = (Mathf.Floor(frac * 200)) / 200;
+			if(fraction*res.maxAmount < res.minAmount) 
+				frac = (float)(res.minAmount/res.maxAmount);
 			GUILayout.EndVertical();
-			// amount and capacity
-			GUILayout.Box((Math.Round(pool-fraction*maxAmount, 2)).ToString(),
-						   Styles.white, GUILayout.Width(75),
-						   GUILayout.Height(40));
-			GUILayout.Box((Math.Round(fraction*maxAmount, 2)).ToString(),
-						   Styles.fracStyle(fraction), GUILayout.Width(75),
-						   GUILayout.Height(40));
-			GUILayout.Box((Math.Round(capacity, 2)).ToString(),
-						   Styles.yellow, GUILayout.Width(75),
-						   GUILayout.Height(40));
+			// rhs amount and capacity
+			GUILayout.Box(Utils.formatBigValue((float)(rhs), "u"),
+			              Styles.fracStyle((float)(rhs/res.capacity)), 
+			              GUILayout.Width(60), GUILayout.Height(res_line_height));
+			GUILayout.Box(Utils.formatBigValue((float)res.capacity, "u"),
+			              Styles.yellow, GUILayout.Width(60), GUILayout.Height(res_line_height));
 			GUILayout.FlexibleSpace();
 			GUILayout.EndHorizontal();
-			return fraction;
+			return frac;
 		}
 		
 		void TransferWindow(int windowId)
@@ -75,12 +75,14 @@ namespace AT_Utils
 			GUILayout.BeginVertical();
 			link_lfo_sliders = GUILayout.Toggle(link_lfo_sliders, "Link LiquidFuel and Oxidizer sliders");
 			var nres = transfer_list.Count;
-			scroll = GUILayout.BeginScrollView(scroll, GUILayout.Height(res_line_height*Math.Max(nres, 4)));
+			scroll = GUILayout.BeginScrollView(scroll, 
+			                                   GUILayout.Width(width),
+			                                   GUILayout.Height(res_line_height*Math.Min(nres, 4)));
 			for(int i = 0; i < nres; i++)
 			{
 				var r = transfer_list[i];
 				float frac = r.maxAmount > 0 ? (float)(r.amount / r.maxAmount) : 0f;
-				frac = ResourceLine(r.name, frac, r.pool, r.minAmount, r.maxAmount, r.capacity);
+				frac = ResourceLine(r.name, frac, r);
 				if(link_lfo_sliders && (r.name == "LiquidFuel" || r.name == "Oxidizer"))
 				{
 					string other = r.name == "LiquidFuel" ? "Oxidizer" : "LiquidFuel";
@@ -101,9 +103,8 @@ namespace AT_Utils
 		
 		public void Draw(string title, List<ResourceManifest> resourceTransferList)
 		{
-			if(resourceTransferList.Count == 0) return;
-			transfer_list = resourceTransferList;
 			LockControls();
+			transfer_list = resourceTransferList;
 			WindowPos = GUILayout.Window(GetInstanceID(), 
 			                             WindowPos, TransferWindow,
 			                             title, 
@@ -112,4 +113,3 @@ namespace AT_Utils
 		}
 	}
 }
-
