de42b956ec39853fcfbc44809bae2f6d864dd1e8
Allis Tauri <allista@gmail.com>
2016-12-27 19:33:33 +0300

    Moved debug routines into Debugging. Added ResourceHack module to replenish resources in flight.

---
+++ Debugging/ResourceHack.cs
@@ -0,0 +1,73 @@
+﻿#if DEBUG
+//   ResourceHack.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2016 Allis Tauri
+using UnityEngine;
+
+namespace AT_Utils
+{
+	public class ResourceHack : PartModule
+	{
+		bool manage;
+
+		[KSPEvent(guiName = "Manage Resources", guiActive = true, guiActiveUncommand = true)]
+		void ManageResources() { manage = !manage; }
+
+		public override void OnAwake()
+		{
+			base.OnAwake();
+			LockName += GetInstanceID().ToString();
+		}
+
+		public override void OnStart(StartState state)
+		{
+			base.OnStart(state);
+			StartCoroutine(CallbackUtil.DelayedCallback(5, () => { enabled = isEnabled = part.Resources.Count > 0; }));
+		}
+
+		string LockName = "ResourceHack";
+		static int width = 200, height = 150;
+		Vector2 scroll = Vector3.zero;
+		Rect WindowPos = new Rect((Screen.width-width)/2, Screen.height/4, width, height+60);
+		void ResourceManager(int windowID)
+		{
+			GUILayout.BeginVertical();
+			scroll = GUILayout.BeginScrollView(scroll, Styles.white, GUILayout.Width(width), GUILayout.Height(height));
+			foreach(var res in part.Resources)
+			{
+				GUILayout.BeginHorizontal();
+				GUILayout.Label(res.resourceName, Styles.boxed_label, GUILayout.ExpandWidth(true));
+				if(GUILayout.Button("Empty", Styles.danger_button, GUILayout.ExpandWidth(false)))
+					res.amount = 0;
+				if(GUILayout.Button("Fill", Styles.enabled_button, GUILayout.ExpandWidth(false)))
+					res.amount = res.maxAmount;
+				GUILayout.EndHorizontal();
+			}
+			GUILayout.EndScrollView();
+			if(GUILayout.Button("Close", Styles.danger_button, GUILayout.ExpandWidth(true)))
+				manage = false;
+			GUILayout.EndVertical();
+			GUIWindowBase.TooltipsAndDragWindow(WindowPos);
+		}
+
+		void OnGUI()
+		{
+			
+			if(Event.current.type != EventType.Layout && Event.current.type != EventType.Repaint) return;
+			if(manage && GUIWindowBase.HUD_enabled)
+			{
+				Styles.Init();
+				Utils.LockIfMouseOver(LockName, WindowPos);
+				WindowPos = GUILayout.Window(GetInstanceID(), 
+				                             WindowPos, ResourceManager, part.partInfo.title,
+			                                 GUILayout.Width(width),
+			                                 GUILayout.Height(height)).clampToScreen();
+			}
+			else Utils.LockIfMouseOver(LockName, WindowPos, false);
+		}
+	}
+}
+#endif
