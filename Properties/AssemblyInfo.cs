28613902294930cff5f96fe6d35726447bf4ceea
Allis Tauri <allista@gmail.com>
2016-12-27 19:38:04 +0300

    Updated version. v1.3.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,7 +24,7 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyVersion("1.3.1")]
 [assembly: KSPAssembly("AT_Utils", 1, 3)]
 
 // The following attributes are used to specify the signing key for the assembly,
