43be4c6fcc06f1c580a4f1b2497c2782d124d3a1
Allis Tauri <allista@gmail.com>
2017-07-03 23:15:53 +0300

    Added HierarchicalComparer and ValueFieldBase classes.
    HC is a container for optimization constrains of different importance.
    
    VFB is the base of all field widgets; it adds an important ability to set value
    with Return key.

---
+++ ControlFlow/HierarchicalComparer.cs
@@ -0,0 +1,90 @@
+﻿//   HierarchicalContition.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2017 Allis Tauri
+
+using System;
+using System.Collections.Generic;
+
+namespace AT_Utils
+{
+    /// <summary>
+    /// This is a composite boolean comparer that answers the question "is X better than Y?"
+    /// using a hierarchy of conditions. It is designed to encapsulate multiple constrains of an 
+    /// optimization problem, which have different "importance".
+    /// 
+    /// A condition itself can answer one of the two questions:
+    /// 
+    /// 1. Is X good or bad? If X is bad and Y is bad, which is better?
+    /// If "X is good", then the next condition in the hierarchy is checked.
+    /// If "X is NOT good", than X is compared with Y and the result is returned.
+    /// 
+    /// 2. Which is better, X or Y?
+    /// The X is compared with Y and the result is returned, 
+    /// so conditions below it in the hierarchy will NEVER be checked.
+    /// </summary>
+    public class HierarchicalComparer<T>
+    {
+        public delegate bool Constraint(T x);
+        public delegate bool Comparison(T x, T y);
+
+        public struct Condition
+        {
+            public Constraint isGood;
+            public Comparison isBetter;
+        }
+
+        List<Condition> conditions = new List<Condition>();
+
+        public static Condition MakeCondition(Constraint isGood, Comparison isBetter)
+        {
+            return new Condition{isGood = isGood, isBetter = isBetter};
+        }
+
+        public HierarchicalComparer() {}
+        public HierarchicalComparer(params Condition[] many_conditions)
+        {
+            AddConditions(many_conditions);
+        }
+
+        public void AddCondition(Constraint isGood, Comparison isBetter)
+        {
+            AddCondition(MakeCondition(isGood, isBetter));
+        }
+
+        public void AddCondition(Condition condition)
+        {
+            if(condition.isBetter == null)
+                throw new ArgumentException("condition.isBetter cannot be null");
+            conditions.Add(condition);
+        }
+
+        public void AddConditions(params Condition[] many_conditions)
+        {
+            for(int i = 0, len = many_conditions.Length; i < len; i++)
+                AddCondition(many_conditions[i]);
+        }
+
+        public void Clear() { conditions.Clear(); }
+
+        public bool isBetter(T x, T y)
+        {
+            if(x.Equals(y)) return false;
+            for(int i = 0, count = conditions.Count; i < count; i++)
+            {
+                var c = conditions[i];
+                if(c.isGood == null)
+                    return c.isBetter(x, y);
+                if(c.isGood(x))
+                    continue;
+                if(c.isGood(y))
+                    return false;
+                return c.isBetter(x, y);
+            }
+            return true;
+        }
+    }
+}
+
---
+++ GUI/Elements/ValueFieldBase.cs
@@ -0,0 +1,90 @@
+﻿//   ValueFieldBase.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2017 Allis Tauri
+
+using UnityEngine;
+
+namespace AT_Utils
+{
+    public abstract class ValueFieldBase<T> : ConfigNodeObject, ITypeUI<T>
+    {
+        protected string svalue;
+        [Persistent] public T _value;
+        [Persistent] public string format;
+
+        string _field_name;
+        protected string field_name 
+        {
+            get 
+            {
+                if(string.IsNullOrEmpty(_field_name))
+                    _field_name = "filed_"+GetHashCode().ToString("D");
+                return _field_name;
+            }
+        }
+
+        protected ValueFieldBase(string format) 
+        { 
+            this.format = format;
+        }
+
+        public override void Load(ConfigNode node)
+        {
+            base.Load(node);
+            Value = _value;
+        }
+
+        protected abstract string formated_value { get ; }
+
+        public virtual T Value
+        {
+            get { return _value; }
+            set
+            {
+                _value = value;
+                svalue = formated_value;
+            }
+        }
+
+        public bool IsSet { get; protected set; }
+
+        public abstract void Invert();
+        public abstract bool UpdateValue();
+
+        protected bool TrySetValue()
+        {
+            if(Event.current.isKey && 
+               Event.current.keyCode == KeyCode.Return && 
+               GUI.GetNameOfFocusedControl() == field_name)
+                IsSet = UpdateValue();
+            return IsSet;
+        }
+
+        public abstract bool Draw();
+
+        public override string ToString() { return formated_value; }
+
+        public static implicit operator T(ValueFieldBase<T> fi) { return fi._value; }
+    }
+
+    public abstract class BoundValueField<T> : ValueFieldBase<T>
+    {
+        [Persistent] public T Min;
+        [Persistent] public T Max;
+        [Persistent] public bool Circle;
+
+        protected BoundValueField(string format, T min, T max, bool circle) : base(format)
+        {
+            Circle = circle;
+            Min = min; Max = max;
+            Value = _value;
+        }
+
+        public abstract T Range { get; }
+        public abstract void ClampValue();
+    }
+}
+
