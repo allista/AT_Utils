ad61296168e24c7db248033e61faa1f254d60011
Allis Tauri <allista@gmail.com>
2018-05-28 00:34:49 +0300

    Added ATGroundAnchor module that combines GC's and Hangar's anchors.
    Removed ModuleAsteroidFix

---
+++ Modules/ATGroundAnchor.cs
@@ -0,0 +1,140 @@
+﻿//   ModuleGroundAnchor.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2018 Allis Tauri
+
+using UnityEngine;
+
+namespace AT_Utils
+{
+    public class ATGroundAnchor : PartModule
+    {
+		[KSPField] bool Controllable = true;
+        [KSPField (isPersistant = true)] bool isAttached;
+
+        [KSPField] public string attachSndPath = string.Empty;
+        [KSPField] public string detachSndPath = string.Empty;
+        public FXGroup fxSndAttach, fxSndDetach;
+
+        FixedJoint anchorJoint;
+        GameObject anchor;
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+            if(!string.IsNullOrEmpty(attachSndPath))
+                Utils.createFXSound(part, fxSndAttach, attachSndPath, false);
+            if(!string.IsNullOrEmpty(detachSndPath))
+                Utils.createFXSound(part, fxSndDetach, detachSndPath, false);
+            if(isAttached)
+                setup_ground_contact();
+            update_part_events();
+        }
+
+        void OnPartPack() => detatch_anchor();
+        void OnPartUnpack()
+        {
+            if(isAttached)
+            {
+				setup_ground_contact();
+                ForceAttach();
+            }
+        }
+
+        void Update()
+        {
+            if(isAttached)
+            {
+                setup_ground_contact();
+                if(!anchor || !anchorJoint || !anchor.GetComponent<FixedJoint>())
+                    ForceAttach();
+                else 
+                    DumpVelocity();
+            }
+        }
+
+        void setup_ground_contact()
+        {
+            part.PermanentGroundContact = true;
+            if(vessel != null) vessel.permanentGroundContact = true;
+        }
+
+        void detatch_anchor()
+        {
+            if(anchor) Destroy(anchor);
+            if(anchorJoint) Destroy(anchorJoint);
+        }
+
+        bool can_attach()
+        {
+            //always check relative velocity and acceleration
+            if(!vessel.Landed) 
+            {
+                Utils.Message("There's nothing to attach the anchor to");
+                return false;
+            }
+            if(vessel.GetSrfVelocity().sqrMagnitude > 1f)
+            {
+                Utils.Message("Cannot attach the anchor while mooving");
+                return false;
+            }
+            return true;
+        }
+
+        void update_part_events()
+        {
+            Events["Attach"].active = Controllable && !isAttached;
+            Events["Detach"].active = Controllable && isAttached;
+        }
+
+        public void DumpVelocity()
+        {
+            if(vessel == null || !vessel.loaded) return;
+            for(int i = 0, nparts = vessel.parts.Count; i < nparts; i++)
+            {
+                var r = vessel.parts[i].Rigidbody;
+                if(r == null) continue;
+                r.angularVelocity *= 0;
+                r.velocity *= 0;
+            }
+        }
+
+        public void ForceAttach()
+        {
+            detatch_anchor();
+            DumpVelocity();
+            anchor = new GameObject("AnchorBody");
+            var rb = anchor.AddComponent<Rigidbody>();
+            rb.isKinematic = true;
+            anchor.transform.position = part.transform.position;
+            anchor.transform.rotation = part.transform.rotation;
+            anchorJoint = anchor.AddComponent<FixedJoint>();
+            anchorJoint.breakForce = float.PositiveInfinity;
+            anchorJoint.breakTorque = float.PositiveInfinity;
+            anchorJoint.connectedBody = part.Rigidbody;
+            if(!isAttached && fxSndAttach.audio != null) 
+                fxSndAttach.audio.Play();
+            isAttached = true;
+            update_part_events();
+        }
+
+        [KSPEvent (guiActive = true, guiName = "Attach Anchor", active = true)]
+        public void Attach()
+        {
+            if(can_attach()) 
+                ForceAttach();
+        }
+
+        [KSPEvent (guiActive = true, guiName = "Detach Anchor", active = false)]
+        public void Detach()
+        {
+            detatch_anchor();
+            if(isAttached && fxSndDetach.audio != null) 
+                fxSndDetach.audio.Play();
+            isAttached = false;
+            update_part_events();
+        }
+    }
+}
