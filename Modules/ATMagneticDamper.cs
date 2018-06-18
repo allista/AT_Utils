5479eb2b9ab6816db11f1969545e14fb43e3d11e
Allis Tauri <allista@gmail.com>
2018-06-18 09:25:30 +0300

    Fixed magnetic damper operation; added magnet location and part tags filter

--- Modules/ATMagneticDamper.cs
+++ Modules/ATMagneticDamper.cs
@@ -8,10 +8,13 @@ using UnityEngine;
 
 namespace AT_Utils
 {
-    public class ATMagneticDamper: PartModule
+    public class ATMagneticDamper : PartModule
     {
         [KSPField] public string Sensor = string.Empty;
+        [KSPField] public string MagnetLocation = string.Empty;
+        [KSPField] public string AffectedPartTags = string.Empty;
         [KSPField] public float Attenuation = 0.5f;
+        protected Damper damper;
 
         public override void OnStart(StartState state)
         {
@@ -22,30 +25,79 @@ namespace AT_Utils
                 if(sensor != null)
                 {
                     sensor.AddCollider(true);
-                    var damper = sensor.gameObject.AddComponent<Damper>();
-                    damper.K = 1-Utils.Clamp(Attenuation, 0, 0.999f);
+                    damper = sensor.gameObject.AddComponent<Damper>();
+                    damper.Init(part, Attenuation, MagnetLocation, AffectedPartTags);
                 }
             }
         }
-  
-        class Damper : MonoBehaviour
+
+        public void Enable(bool enable = true)
         {
+            if(damper != null)
+                damper.enabled = enable;
+        }
+
+        protected class Damper : MonoBehaviour
+        {
+            Part host;
+            Transform magnet;
+            string[] tags;
             public float K;
+
+            public void Init(Part part, float attenuation, string magnet = null, string tags = "")
+            {
+                host = part;
+                K = 1 - Utils.Clamp(attenuation, 0, 0.999f);
+                if(!string.IsNullOrEmpty(magnet))
+                    this.magnet = host.FindModelTransform(magnet);
+                if(this.magnet == null)
+                    this.magnet = transform;
+                if(!string.IsNullOrEmpty(tags))
+                    this.tags = Utils.ParseLine(tags, Utils.Comma);
+            }
 
             void OnTriggerStay(Collider col)
             {
-                if(col != null && col.attachedRigidbody != null)
+                if(host != null && host.Rigidbody != null
+                   && col != null && col.attachedRigidbody != null)
                 {
                     if(col.CompareTag("Untagged"))
                     {
                         var p = col.attachedRigidbody.GetComponent<Part>();
                         if(p != null && p.vessel != null && p.vessel.loaded)
                         {
+                            if(tags != null && tags.Length > 0)
+                            {
+                                var tagged = false;
+                                foreach(var t in tags)
+                                {
+                                    if(p.partInfo.tags.Contains(t))
+                                    {
+                                        tagged = true;
+                                        break;
+                                    }
+                                }
+                                if(!tagged)
+                                    return;
+                            }
                             var r = p.Rigidbody;
+                            var h = host.Rigidbody;
                             if(r != null)
                             {
-                                r.angularVelocity *= K;
-                                r.velocity *= K;
+                                // damp angular and linear velocity
+                                var dI = (r.velocity - h.velocity) * r.mass * K;
+                                var dAv = (h.angularVelocity - r.angularVelocity) * K;
+                                r.AddTorque(dAv, ForceMode.VelocityChange);
+                                r.AddForce(-dI, ForceMode.Impulse);
+                                h.AddForce(dI, ForceMode.Impulse);
+                                // add force to attract the part to magnet's center
+                                if(magnet != null)
+                                {
+                                    var d = magnet.position - r.worldCenterOfMass;
+                                    var attraction = (d.sqrMagnitude > 1? d.normalized : d) * r.mass;
+                                    r.AddForce(attraction, ForceMode.Force);
+                                    h.AddForce(-attraction, ForceMode.Force);
+                                }
                             }
                         }
                     }
