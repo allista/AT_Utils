517d3cf2631a9d3bc29e44ec1ad68c55db23140a
Evgeny Taranov <taranov@jet.msk.su>
2018-10-16 20:07:17 +0300

    Added PartMaker to make a part from the name and a ShipConstruct from the part
    Added PartSelector: like SubassemblySelector, but selects AvailableParts

---
+++ Addons/PartMaker.cs
@@ -0,0 +1,86 @@
+﻿//   ShipMaker.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2018 Allis Tauri
+using UnityEngine;
+
+namespace AT_Utils
+{
+    public static class PartMaker
+    {
+        public static Part CreatePart(string part_name, string flag_url)
+        {
+            var part_info = PartLoader.getPartInfoByName(part_name);
+            if(part_info == null)
+            {
+                Utils.Message("No such part: {0}", part_name);
+                return null;
+            }
+            return CreatePart(part_info, flag_url);
+        }
+
+        public static Part CreatePart(AvailablePart part_info, string flag_url)
+        {
+            var part = Object.Instantiate(part_info.partPrefab);
+            part.gameObject.SetActive(true);
+            part.partInfo = part_info;
+            part.name = part_info.name;
+            part.flagURL = flag_url;
+            part.persistentId = FlightGlobals.GetUniquepersistentId();
+            FlightGlobals.PersistentLoadedPartIds.Remove(part.persistentId);
+            part.transform.position = Vector3.zero;
+            part.attPos0 = Vector3.zero;
+            part.transform.rotation = Quaternion.identity;
+            part.attRotation = Quaternion.identity;
+            part.attRotation0 = Quaternion.identity;
+            part.partTransform = part.transform;
+            part.orgPos = part.transform.root.InverseTransformPoint(part.transform.position);
+            part.orgRot = Quaternion.Inverse(part.transform.root.rotation) * part.transform.rotation;
+            part.packed = true;
+            //initialize modules
+            part.InitializeModules();
+            var module_nodes = part_info.partConfig.GetNodes("MODULE");
+            for(int i = 0, maxLength = module_nodes.Length; i < maxLength; i++)
+            {
+                var node = module_nodes[i];
+                var module_name = node.GetValue("name");
+                if(!string.IsNullOrEmpty(module_name))
+                {
+                    var module = part.Modules[i];
+                    if(module != null && module.ClassName == module_name)
+                        module.Load(node);
+                }
+            }
+            foreach(var module in part.Modules)
+                module.OnStart(PartModule.StartState.PreLaunch);
+            return part;
+        }
+
+        public static ShipConstruct CreatePartConstruct(Part part, string name = null, string description = null)
+        {
+            var title = part.Title();
+            var ship = new ShipConstruct(name ?? title, description ?? title, part)
+            {
+                rotation = Quaternion.identity,
+                missionFlag = part.flagURL
+            };
+            return ship;
+        }
+
+        public static ShipConstruct CreatePartConstruct(string part_name, string flag_url,
+                                                        string name = null, string description = null)
+        {
+            var part = CreatePart(part_name, flag_url);
+            return part != null ? CreatePartConstruct(part, name, description) : null;
+        }
+
+        public static ShipConstruct CreatePartConstruct(AvailablePart part_info, string flag_url,
+                                                        string name = null, string description = null)
+        {
+            var part = CreatePart(part_info, flag_url);
+            return part != null ? CreatePartConstruct(part, name, description) : null;
+        }
+    }
+}
---
+++ GUI/Windows/PartSelector.cs
@@ -0,0 +1,111 @@
+﻿//   PartSelector.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2018 Allis Tauri
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AT_Utils
+{
+    public class PartSelector : GUIWindowBase
+    {
+        List<AvailablePart> parts = new List<AvailablePart>();
+        List<GUIContent> buttons = new List<GUIContent>();
+        Action<AvailablePart> load_part = delegate { };
+
+        public PartSelector()
+        {
+            width = 400;
+            height = 200;
+            WindowPos = new Rect(Screen.width / 2 - width / 2, 100, width, 100);
+        }
+
+        public override void Awake()
+        {
+            base.Awake();
+            Show(false);
+        }
+
+        public void RefreshParts()
+        {
+            parts.Clear();
+            buttons.Clear();
+            if(PartLoader.LoadedPartsList == null) return;
+            foreach(var info in PartLoader.LoadedPartsList)
+            {
+                if(Utils.PartIsPurchased(info))
+                {
+                    Vector3 dims = Vector3.zero;
+                    float mass = float.NaN, cost = float.NaN;
+                    if(info.partPrefab)
+                    {
+                        mass = info.partPrefab.TotalMass();
+                        cost = info.partPrefab.TotalCost();
+                        dims = new Metric(info.partPrefab).size;
+                    }
+                    var label = string.Format("<color=yellow><b>{0}</b></color>\n" +
+                                              "<color=silver>mass:</color> {1:F1}t " +
+                                              "<color=silver>cost:</color> {2:F0} " +
+                                              "<color=silver>size:</color> {3}",
+                                              info.title, mass, cost,
+                                              Utils.formatDimensions(dims));
+                    var button = new GUIContent(label, info.description);
+                    buttons.Add(button);
+                    parts.Add(info);
+                }
+            }
+        }
+
+        public override void Show(bool show)
+        {
+            base.Show(show);
+            if(show) RefreshParts();
+        }
+
+        Vector2 scroll = Vector2.zero;
+        void DrawWindow(int windowID)
+        {
+            GUILayout.BeginVertical();
+            scroll = GUILayout.BeginScrollView(scroll);
+            AvailablePart toLoad = null;
+            for(int i = 0, count = buttons.Count; i < count; i++)
+            {
+                var button = buttons[i];
+                GUILayout.BeginHorizontal();
+                if(GUILayout.Button(button, Styles.boxed_label, GUILayout.ExpandWidth(true)))
+                    toLoad = parts[i];
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.EndScrollView();
+            if(toLoad != null && load_part != null)
+            {
+                load_part(toLoad);
+                Show(false);
+            }
+            if(GUILayout.Button("Close", Styles.close_button, GUILayout.ExpandWidth(true)))
+                Show(false);
+            GUILayout.EndVertical();
+            TooltipsAndDragWindow();
+        }
+
+        public void Draw(Action<AvailablePart> loadPart)
+        {
+            if(doShow)
+            {
+                LockControls();
+                load_part = loadPart;
+                WindowPos = GUILayout.Window(GetInstanceID(),
+                                             WindowPos,
+                                             DrawWindow,
+                                             "Select Part",
+                                             GUILayout.Width(width),
+                                             GUILayout.Height(height))
+                                     .clampToScreen();
+            }
+            else UnlockControls();
+        }
+    }
+}
