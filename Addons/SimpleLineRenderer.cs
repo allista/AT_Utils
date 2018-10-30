5be32d5497bbe31930f753f8f7e22fbc02001cfe
Allis Tauri <allista@gmail.com>
2018-10-30 20:36:14 +0300

    Working SimpleLineRenderer.

--- Addons/SimpleLineRenderer.cs
+++ Addons/SimpleLineRenderer.cs
@@ -5,81 +5,147 @@
 //
 //  Copyright (c) 2018 Allis Tauri
 using System.Collections.Generic;
-using System.Linq;
 using UnityEngine;
 using Vectrosity;
 namespace AT_Utils
 {
-    public class SimpleLineRenderer : MonoBehaviour
+    public class SimpleLineRenderer
     {
+        protected string name = "line";
+        protected float width = 5f;
+
         VectorLine line;
         Vector3d[] points;
+        List<Color32> colors;
+        Color color = Color.gray;
 
-        public float alpha = 1f;
-        public Color color = Color.gray;
-        protected int layerMask = 31;
+        public int NumPoints => points != null ? points.Length : -1;
 
-        protected virtual Color line_color => color.A(alpha);
-
-        public bool isActive => line != null && line.active;
-
-        protected virtual void create_line()
+        public bool isActive
         {
-            if(points.Length > 0)
+            get { return line != null && line.active; }
+            set { if(line != null) line.active = value; }
+        }
+
+        public SimpleLineRenderer(string name, float width = 5f)
+        {
+            this.name = name;
+            this.width = width;
+        }
+
+        ~SimpleLineRenderer()
+        {
+            destroy_line();
+        }
+
+        protected virtual void destroy_line()
+        {
+            if(line != null)
             {
-                line = new VectorLine(name + " line", new List<Vector3>(points.Length), 5f, LineType.Continuous);
+                line.active = false;
+                VectorLine.Destroy(ref line);
+                line = null;
+            }
+        }
+
+        protected virtual bool create_line()
+        {
+            if(line == null || points.Length != line.points3.Count)
+            {
+                destroy_line();
+#pragma warning disable IDE0017 // Simplify object initialization
+                line = new VectorLine(name, new List<Vector3>(points.Length), width,
+                                      LineType.Continuous, Joins.Fill);
+#pragma warning restore IDE0017 // Simplify object initialization
                 line.texture = MapView.OrbitLinesMaterial.mainTexture;
                 line.material = MapView.OrbitLinesMaterial;
                 line.continuousTexture = true;
-                line.color = line_color;
-                line.rectTransform.gameObject.layer = layerMask;
+                line.ContinuousTextureOffset = 0;
+                line.smoothColor = true;
                 line.UpdateImmediate = true;
+                scaled_space = MapView.MapIsEnabled;
+                line.rectTransform.gameObject.layer = scaled_space? 31 : 1;
             }
-            else if(line != null)
-                line.active = false;
+            return line != null;
         }
 
+        bool scaled_space;
         protected virtual void update()
         {
+            line.rectTransform.position = Vector3.zero;
             if(MapView.MapIsEnabled)
+            {
+                if(!scaled_space)
+                {
+                    destroy_line();
+                    create_line();
+                    scaled_space = true;
+                }
                 ScaledSpace.LocalToScaledSpace(points, line.points3);
+            }
             else
             {
-                for(int i = 0, pointsLength = points.Length; i < pointsLength; i++)
+                if(scaled_space)
                 {
-                    line.points3[i] = points[i];
-                    line.drawEnd = points.Length - 1;
+                    destroy_line();
+                    create_line();
+                    scaled_space = false;
                 }
+                for(int i = 0, pointsLength = points.Length; i < pointsLength; i++)
+                    line.points3[i] = points[i];
+            }
+            update_colors();
+            line.Draw3D();
+        }
+
+        void update_colors()
+        {
+            if(colors != null)
+                line.SetColors(colors);
+            else
+                line.SetColor(color);
+        }
+
+        public virtual void SetPoints(Vector3d[] points)
+        {
+            this.points = points;
+            if(points == null || points.Length == 0)
+                Reset();
+            else if(create_line())
+            {
+                update();
+                line.active = true;
             }
         }
 
-        protected virtual void OnDestroy()
+        public virtual void SetPoints(Vector3d[] points, Color color)
         {
-            if(line != null)
-                VectorLine.Destroy(ref line);
+            this.color = color;
+            colors = null;
+            SetPoints(points);
         }
 
-        public virtual void SetPoints(IList<Vector3d> points, Color color, float alpha = 1f)
+        public virtual void SetPoints(Vector3d[] points, List<Color32> colors)
         {
-            this.points = points.ToArray();
-            this.color = color;
-            this.alpha = alpha;
-            create_line();
+            this.colors = null;
+            if(points != null && colors != null)
+            {
+                if(colors.Count == points.Length - 1)
+                    this.colors = colors;
+                else
+                    Utils.Log("SimpleLineRenderer[{}] Number of colors should be equal " +
+                              "to points.Length-1. Expected {} got {}",
+                              name, points.Length - 1, colors.Count);
+            }
+            SetPoints(points);
         }
 
         public virtual void Reset()
         {
             points = null;
-            create_line();
-        }
-
-        protected virtual void LateUpdate()
-        {
-            if(isActive)
-            {
-                update();
-                line.Draw();
-            }
+            colors = null;
+            destroy_line();
         }
     }
 }
+
