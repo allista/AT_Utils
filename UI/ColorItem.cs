99a1c7b9a144521059f90d16e30f0ed274d7a793
Allis Tauri <allista@gmail.com>
2018-11-08 22:19:52 +0300

    fixup! Moved UI project to be able to use .dll in prefabs

---
+++ UI/ColorItem.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace AT_Utils.UI
+{
+    public class ColorItem : MonoBehaviour
+    {
+        [SerializeField]
+        Image colorDisplay;
+
+        [SerializeField]
+        InputField colorHTML;
+
+        [SerializeField]
+        Text pickerToggleText;
+
+        public ColorPicker picker;
+        public Toggle pickerToggle;
+
+        IColored colored;
+
+        void Awake()
+        {
+            colorHTML.readOnly = true;
+            pickerToggle.onValueChanged.AddListener(togglePicker);
+        }
+
+        public void SetColored(IColored colored, string name)
+        {
+            this.colored = colored;
+            pickerToggleText.text = name;
+            ChangeColor(colored.color);
+        }
+
+        public void ChangeColor(Color color)
+        {
+            if(colored != null)
+                colored.color = color;
+            colorDisplay.color = color;
+            colorHTML.text = "#" + ColorUtility.ToHtmlStringRGBA(color);
+        }
+
+        void togglePicker(bool toggle)
+        {
+            if(picker == null) return;
+            if(toggle)
+            {
+                picker.CurrentColor = colorDisplay.color;
+                picker.onValueChanged.AddListener(ChangeColor);
+            }
+            else
+                picker.onValueChanged.RemoveListener(ChangeColor);
+            if(pickerToggle.group != null)
+                picker.gameObject.SetActive(pickerToggle.group.AnyTogglesOn());
+            else
+                picker.gameObject.SetActive(toggle);
+        }
+    }
+}
---
+++ UI/ColorList.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace AT_Utils.UI
+{
+    public class ColorList : DragablePanel
+    {
+        [SerializeField]
+        ColorPicker picker;
+
+        [SerializeField]
+        GameObject listContainer;
+
+        [SerializeField]
+        GameObject colorItemPrefab;
+
+        [SerializeField]
+        Button cancelButton;
+
+        [SerializeField]
+        Button applyButton;
+
+        [SerializeField]
+        Text title;
+
+        List<ColorItem> items = new List<ColorItem>();
+        ToggleGroup colorItemsGroup;
+
+        protected virtual void Awake()
+        {
+            colorItemsGroup = listContainer.GetComponent<ToggleGroup>();
+        }
+
+        public void SetTitle(string text)
+        {
+            title.text = text;
+        }
+
+        public void AddColored(IColored colored, string name)
+        {
+            if(colored == null) return;
+            var itemObj = Instantiate(colorItemPrefab);
+            var item = itemObj.GetComponent<ColorItem>();
+            item.picker = picker;
+            item.SetColored(colored, name);
+            item.pickerToggle.group = colorItemsGroup;
+            colorItemsGroup.RegisterToggle(item.pickerToggle);
+            itemObj.transform.SetParent(listContainer.transform);
+            items.Add(item);
+        }
+
+        public void AddOnCancel(UnityAction action)
+        {
+            cancelButton.onClick.AddListener(action);
+        }
+
+        public void AddOnApply(UnityAction action)
+        {
+            applyButton.onClick.AddListener(action);
+        }
+
+        void OnDestroy()
+        {
+            items.ForEach(it => Destroy(it.gameObject));
+            items.Clear();
+        }
+    }
+}
