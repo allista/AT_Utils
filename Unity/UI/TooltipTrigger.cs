1993069dceb5d0b303a74059db0e3ea956d1d64a
Allis Tauri <allista@gmail.com>
2019-06-05 23:34:46 +0300

    Added TooltipView+TooltipTrigger framework

---
+++ Unity/UI/TooltipTrigger.cs
@@ -0,0 +1,40 @@
+﻿//   TooltipTrigger.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2019 Allis Tauri
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace AT_Utils.UI
+{
+    public class TooltipTrigger : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        public string text;
+        public float delay = 0.3f;
+        public TooltipView.TooltipPosition position = TooltipView.TooltipPosition.BOTTOM;
+        float enterTime = -1;
+
+        void Update()
+        {
+            if(enterTime > 0 && Time.realtimeSinceStartup - enterTime > delay)
+            {
+                enterTime = -1;
+                TooltipView.Instance?.ShowTooltip(text, position);
+            }
+        }
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            if(!string.IsNullOrEmpty(text))
+                enterTime = Time.realtimeSinceStartup;
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            enterTime = -1;
+            TooltipView.Instance?.HideTooltip();
+        }
+    }
+}
