4d8c7719cea562fd13230d05bb632fddb5c2cd92
Allis Tauri <allista@gmail.com>
2019-06-08 12:54:13 +0300

    Added Extensions class and backported Toggle.SetIsOnWithoutNotify

---
+++ Unity/UI/Extensions.cs
@@ -0,0 +1,30 @@
+﻿//   Extensions.cs
+//
+//  Author:
+//       Allis Tauri <allista@gmail.com>
+//
+//  Copyright (c) 2019 Allis Tauri
+using System.Reflection;
+using UnityEngine.UI;
+
+namespace AT_Utils.UI
+{
+    public static class Extensions
+    {
+        static readonly MethodInfo toggleSet = typeof(Toggle)
+            .GetMethod("Set",
+                       BindingFlags.NonPublic | BindingFlags.Instance,
+                       null,
+                       new[] { typeof(bool), typeof(bool) },
+                       null);
+
+        public static void SetIsOnWithoutNotify(this Toggle toggle, bool isOn)
+        {
+            if(toggle.isOn != isOn)
+            {
+                toggleSet.Invoke(toggle, new object[] { isOn, false });
+                toggle.GetComponent<ToggleColorizer>()?.UpdateColor();
+            }
+        }
+    }
+}
