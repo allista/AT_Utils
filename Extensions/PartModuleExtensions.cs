39d4c15d2e87daa151685e4d6474a45fc4278e48
Allis Tauri <allista@gmail.com>
2020-05-07 11:01:33 +0300

    Added PartModule.Log(string) extension to avoid props array allocation

--- Extensions/PartModuleExtensions.cs
+++ Extensions/PartModuleExtensions.cs
@@ -5,8 +5,7 @@ namespace AT_Utils
         public static string Title(this PartModule pm) =>
             pm.part.partInfo != null ? pm.part.partInfo.title : pm.part.name;
 
-        public static void EnableModule(this PartModule pm, bool enable) =>
-            pm.enabled = pm.isEnabled = enable;
+        public static void EnableModule(this PartModule pm, bool enable) => pm.enabled = pm.isEnabled = enable;
 
         public static void ConfigurationInvalid(
             this PartModule pm,
@@ -20,6 +19,8 @@ namespace AT_Utils
                 pm.Title());
             pm.enabled = pm.isEnabled = false;
         }
+
+        public static void Log(this PartModule pm, string msg) => Utils.Log($"{pm.GetID()}: {msg}");
 
         public static void Log(this PartModule pm, string msg, params object[] args) =>
             Utils.Log($"{pm.GetID()}: {msg}", args);
