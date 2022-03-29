3f616a4d7a12b6872815b52bf7f5a0b875f28a58
Allis Tauri <allista@gmail.com>
2022-03-29 13:53:04 +0300

    KSP: MinMaxVersion: 1.12.3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -43,8 +43,8 @@ namespace AT_Utils
     {
         public ModInfo()
         {
-            MinKSPVersion = new Version(1, 11, 1);
-            MaxKSPVersion = new Version(1, 11, 1);
+            MinKSPVersion = new Version(1, 12, 3);
+            MaxKSPVersion = new Version(1, 12, 3);
 
             VersionURL = "https://raw.githubusercontent.com/allista/AT_Utils/master/GameData/000_AT_Utils/000_AT_Utils.version";
             UpgradeURL = "https://github.com/allista/AT_Utils/releases";
